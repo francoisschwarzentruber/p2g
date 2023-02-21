5064948a232d31cbf9b7aa284354fe6458592756
Francois Schwarzentruber <fschwarz@schumann.irisa.fr>
2023-02-21 22:08:24 +0100

    example

--- p2g.py
+++ p2g.py
@@ -1,9 +1,13 @@
 import re
 from collections import OrderedDict
 import os
+import sys
 
 class Proof:
 	def __init__(self, isWithHook = False):
+		"""
+		isWithHook: True if we add a special vertex for the hook of the proof (linked to the corresponding theorem statement)
+		"""
 		self.nodes = OrderedDict()
 		if isWithHook:
 			idHook = generateId()
@@ -13,11 +17,19 @@ class Proof:
 		self.lastid = None
 		
 
-	def addStatement(self, id, statement):
+	def addStatement(self, id, statement, causeIds):
+		"""
+		id (string) of the statement
+		statement: string
+		causeIds: array of ids
+		"""
 		self.nodes[id] = statement.strip()
 		if self.lastid != None:
-			self.addEdge(self.lastid, id)
+			self._addEdge(self.lastid, id)
 		self.lastid = id
+
+		for byid in causeIds:
+			self._addEdge(byid, id)
 
 
 	def addProof(self, id, proof):
@@ -28,7 +40,7 @@ class Proof:
 		self.nodes[id] = proof
 		self.lastid = None
 
-	def addEdge(self, id1, id2):
+	def _addEdge(self, id1, id2):
 		self.edges.append((id1, id2))
 
 	def toDot(self):
@@ -39,6 +51,7 @@ class Proof:
 				lines.append(f"subgraph cluster_{key} {{")
 				lines.append(l)
 				lines.append("color=blue")
+				lines.append('style="fill=blue!10!white, opacity=0.3"')
 				lines.append("}")
 			elif value == "":
 				lines.append(f'{key}  [label="" color="blue"];')
@@ -46,13 +59,16 @@ class Proof:
 				lines.append(f'{key}  [label="\\text{{{value}}}"  shape=none];')
 
 		for (id1, id2) in self.edges:
-			lines.append(f'{id1} -> {id2};')
+			lines.append(f'{id1} -> {id2} [style="line width=2"];')
 
 		for (idcluster1, id1, id2) in self.edgesProofOf:
-			lines.append(f'{id2} -> {id1} [lhead="{idcluster1}",dir=none color="blue",style="dashed"];')
+			lines.append(f'{id2} -> {id1} [lhead="{idcluster1}",dir=none color="blue",penwidth=3,style="line width=2,dashed"];')
 
 		return "\n".join(lines)
 
+"""
+generates new Id (string)
+"""
 nextId = 0
 def generateId():
 	global nextId
@@ -64,9 +80,9 @@ def generateId():
 reStatementBy = re.compile(r'(.*)by\s\(((\w|,)*)\)')
 reStatementId = re.compile(r'(.*)\((\w*)\)')
 reStatementById = re.compile(r'(.*)by\s\(((\w|,)*)\)\s*\((\w*)\)')
-print(re.match(reStatementBy, "Contradiction        by (irreducible,aeven,beven)").groups())
-print(re.match(reStatementById, "azeazeaze by (miaou)   (id)  ").groups())
-print(re.match(reStatementId, "Lemma. $a^2$ even implies $a$ even.            (lemma)  ").groups())
+#print(re.match(reStatementBy, "Contradiction        by (irreducible,aeven,beven)").groups())
+#print(re.match(reStatementById, "azeazeaze by (miaou)   (id)  ").groups())
+#print(re.match(reStatementId, "Lemma. $a^2$ even implies $a$ even.            (lemma)  ").groups())
 
 
 def linesToProof(lines, depth = 0):
@@ -78,37 +94,28 @@ def linesToProof(lines, depth = 0):
 		if line == "":
 			pass
 		
-		print(line)
 		if line == "{":
 			proof.addProof(generateId(), linesToProof(lines, depth+1))
 		elif line == "}":
 			return proof
 		else:
-
 			def extractInfo(line):
 				result = re.match(reStatementById, line)
 				if result:
-					return result.groups()[0], result.groups()[3], result.groups()[1]
+					return result.groups()[0], result.groups()[3], result.groups()[1].split(',')
 				else:
 					result = re.match(reStatementBy, line)
 					if result:
-						return result.groups()[0], generateId(), result.groups()[1]
+						return result.groups()[0], generateId(), result.groups()[1].split(',')
 					else: 
 						result = re.match(reStatementId, line)
 						if result:
 							return result.groups()[0], result.groups()[1], []
 						else:
-							return line, generateId(), None
+							return line, generateId(), []
 
 			statement, id, by = extractInfo(line)
-			
-			proof.addStatement(id, statement )
-
-			if by:
-				for byid in by.split(','):
-					proof.addEdge(byid, id)
-			
-
+			proof.addStatement(id, statement, by)
 	return proof
 
 def toTex(innerDot):
@@ -116,7 +123,8 @@ def toTex(innerDot):
 	lines = f.readlines()
 	return "".join(map(lambda line: innerDot if line.strip() == "GRAPHVIZCODE" else line, lines))
 	
-inputFile = open("sqrt2irrational.proof", "r")
+###main
+inputFile = open(sys.argv[1], "r")
 lines = inputFile.readlines()
 inputFile.close()
 proof = linesToProof(lines)
